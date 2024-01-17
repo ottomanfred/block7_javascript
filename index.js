0bb259a4df4d372a5c0912d1d2c337062ba86a30
otto18 <otto@ceramic.global>
2024-01-17 15:01:50 -0500

    add javascript for 3 variables and log strings to console

---
+++ index.js
@@ -0,0 +1,7 @@
+const num1 = 5 * 2; //this is to calculate the first variable using an arithmetic expression
+const num2 = 30 + 10; //the second variable
+const num3 = 50 - 11; //the third variable
+console.log(
+  "You have received this message because you have been chosen to open an important vault. Here is the secret combination:"
+); //print this string to the console
+console.log("The lock combination is: " + num1 + "-" + num2 + "-" + num3); //print the lock combination to the console using string concatenation with variables
